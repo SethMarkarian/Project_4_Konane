8dcd56a1540e7e7c2b40d36341ce1fe45c333d3e
Seth Markarian <seth.h.markarian@gmail.com>
2020-10-24 13:45:07 -0400

    Working on getting legal moves

--- game.py
+++ game.py
@@ -5,3 +5,32 @@ class Game:
         self.last_move_made = ((),())
         self.player_piece = ('X', 'O')
         self.end_game = 0
+    
+    @staticmethod
+    def NORTH(position):
+        return (position, (pos[0] - 2, pos[1]))
+    
+    @staticmethod
+    def SOUTH(position):
+        return (position, (pos[0] + 2, pos[1]))
+    
+    @staticmethod
+    def EAST(position):
+        return (position, (pos[0], pos[1] + 2))
+    
+    @staticmethod
+    def WEST(position):
+        return (position, (pos[0], pos[1] - 2))
+
+    def get_legal_moves(self, current_player)
+        legal_moves = [] #array with all possible moves for current player
+        for i in range(8): #loop through rows
+            for j in range(8): #loop through columns
+                if self.board.rowIndex[i][j] == self.player_piece[current_player]: #if the selected piece belongs to current player
+                    current_position = (i, j) #current position tuple
+                    directions = [self.NORTH, self.SOUTH, self.EAST, self.WEST] #ways player can move
+                    for dir in directions: #check all direction possibilities
+                        possible_move = dir(current_position)
+                        #NEED TO CHECK IF MOVE IS LEGAL????
+
+
